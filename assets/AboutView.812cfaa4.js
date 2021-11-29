642d45e440c0548c155b048e3770ded22983311b
littleSHU <2009002@iisigroup.com>
2021-11-29 11:29:19 +0800

    deploy

---
+++ assets/AboutView.812cfaa4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.dc5f5c88.js";import{o,c as t,a as s}from"./vendor.3b7d1946.js";const _={},a={class:"about"},c=s("h1",null,"This is an about page",-1),n=[c];function r(i,d){return o(),t("div",a,n)}var f=e(_,[["render",r]]);export{f as default};
